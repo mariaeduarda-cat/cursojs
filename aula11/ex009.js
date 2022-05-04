4f87e55d31423a3e440cc40eb5865898edd39721
mariaeduarda-cat <93399922+mariaeduarda-cat@users.noreply.github.com>
2022-05-04 17:36:06 -0300

    atualização

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'Estados Unidos';
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') { //Condição composta  
+    console.log("Você é brasileiro");
+} else {
+    console.log("Você é estrangeiro");
+}
