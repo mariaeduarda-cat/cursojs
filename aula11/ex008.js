093c94385bca9f748671796d9306bb9c109adbaf
mariaeduarda-cat <93399922+mariaeduarda-cat@users.noreply.github.com>
2022-05-09 16:40:55 -0300

    atualização

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,7 +1,15 @@
-var vel = 78;
+var vel = 75;
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
 
 if (vel > 60) { //Condição Simples
     console.log("Você ultrapassou a velocidade permitida. MULTADO!!")
 }
 console.log(`Dirija sempre usando o cinto de segurança!`)
+
+var nasc = 'Brasil';
+console.log(`Você é do(a)${nasc}`);
+if (nasc != 'Brasil') {
+    console.log('Você é estrangeiro!!');
+} else {
+    console.log('Você é brasileiro!!');
+}
