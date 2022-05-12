e3b0949b112214507109ea6559f602adcc36be5e
mariaeduarda-cat <93399922+mariaeduarda-cat@users.noreply.github.com>
2022-05-12 19:04:51 -0300

    atualização

---
+++ aula12/ex012.js
@@ -0,0 +1,22 @@
+/*var agora = new Date()
+var hora = agora.getHours()*/
+var hora = 1;
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora < 12) {
+    console.log("Bom dia.")
+} else if(hora <=18) {
+    console.log("Boa tarde.");
+} else if (hora > 18 && hora <= 23){
+    console.log("Boa noite.");
+} else {
+    console.log("Boa madrugada.");
+}
+
+/*var agora = new Date()
+var ano = agora.getFullYear()
+console.log(`Estamos no ano de ${ano}.`)
+
+var agora = new Date()
+var minutos = agora.getMinutes()
+console.log(`Agora são ${minutos} minutos.`)*/
