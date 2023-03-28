347cbcf385ae469fbdb4fce59a56efe5522d635b
mariaeduarda-cat <93399922+mariaeduarda-cat@users.noreply.github.com>
2023-03-28 14:19:08 -0300

    commit

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -6,13 +6,13 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert("[ERRO] Verifique os dados e tente novamente!");
     } else {
-        var fsex = document.getElementsByName('radsex')
+        var fsex =   document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            genero = 'homem'
+            genero = 'homem'                                                           
             if (idade >=0 && idade < 10) {
                 //Criança
                 img.setAttribute('src', 'foto-bebe-m.jpg')
